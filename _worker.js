7e6c774e257a039bb9fea211f581a80bffb9ee32
oneyibao <143400301+oneyibao@users.noreply.github.com>
2023-08-29 15:55:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'r8mjrm-8787.csb.app'
+      url.hostname = 'fd.shabijichang.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
